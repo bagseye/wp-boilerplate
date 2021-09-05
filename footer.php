ed7db2d380d8a7d84dc2a72a749cf5e3c42888ad
Morgan Baker <morganbakerdesigner@gmail.com>
2021-09-05 09:52:46 +0100

    Added header files

---
+++ footer.php
@@ -0,0 +1,39 @@
+</main>
+
+<footer role="contentinfo">
+    <?php if( has_nav_menu( 'footer' ) ) : ?>
+        <nav aria-label="<?php esc_attr_e( 'Secondary Menu', 'wpboiler' ) ?>">
+            <ul>
+                <?php 
+                wp_nav_menu(
+                    array(
+                        'theme_location'    => 'footer',
+                        'items_wrapp'       => '%3$s',
+                        'container'         => false,
+                        'depth'             => 1, 
+                        'link_before'       => '<span>',
+                        'link_after'        => '</span>',
+                        'fallback_cb'       => false,
+                    )
+                    );
+                ?>
+            </ul>
+        </nav>
+    <?php endif; ?>
+    <div class="copyright">
+        <p>&copy; <?php echo date('Y') ?> <?php echo bloginfo( 'name' ) ?></p>
+        <p><?php 
+        printf(
+            esc_html__( 'Designed & developed by %s', 'wpboiler' ),
+            '<a href="' . esc_url( __( 'https://www.morganbaker.dev', 'wpboiler' ) ) . '">Morgan Baker Development</a>'
+        );
+        ?></p>
+    </div>
+</footer>
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> <?php wpboiler_the_html_classes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php 
+if( function_exists( 'wp_body_open' ) ) {
+	wp_body_open();
+} else {
+	do_action( 'wp_body_open' );
+}
+?>
+<div id="page" class="site">
+
+    <a href="#content" class="skip-link screen-reader-text"><?php esc_html_e( 'Skip to content', 'wpboiler' ); ?></a>
+
+    <?php get_template_part( 'template-parts/header/site-header' ); ?>
+
+    <main id="main" class="site-main" role="main">
