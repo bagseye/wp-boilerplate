203e736022848f252ea39881aa2db601d5f058db
Morgan Baker <morganbakerdesigner@gmail.com>
2021-09-09 23:10:32 +0100

    Remove rich text functions

--- js/wpboiler-block-additional-styles.js
+++ js/wpboiler-block-additional-styles.js
@@ -1,26 +1,31 @@
 /**
-* Add New block styles to sidebar
-*
-*/
-wp.blocks.registerBlockStyle('core/paragraph', {
-    name: 'lead',
-    label: 'Lead',
+ * Add New block styles to sidebar
+ *
+ */
+wp.blocks.registerBlockStyle("core/paragraph", {
+  name: "lead",
+  label: "Lead",
 });
 
-wp.blocks.registerBlockStyle('core/button', {
-    name: 'basic',
-    label: 'Basic',
+wp.blocks.registerBlockStyle("core/button", {
+  name: "basic",
+  label: "Basic",
 });
-
 
 /**
-* Remove block styles from sidebar
-*
-*/
-wp.domReady( () => {
-    wp.blocks.unregisterBlockStyle( 'core/button', 'outline' );
-    wp.blocks.unregisterBlockStyle( 'core/separator', 'wide' );
-    wp.blocks.unregisterBlockStyle( 'core/separator', 'dots' );
-    wp.blocks.unregisterBlockStyle( 'core/quote', 'large' );
-    wp.blocks.unregisterBlockStyle('core/image', 'rounded');
+ * Remove some functionality
+ *
+ */
+wp.domReady(() => {
+  // Remove block styles from sidebar
+  wp.blocks.unregisterBlockStyle("core/button", "outline");
+  wp.blocks.unregisterBlockStyle("core/separator", "wide");
+  wp.blocks.unregisterBlockStyle("core/separator", "dots");
+  wp.blocks.unregisterBlockStyle("core/quote", "large");
+  wp.blocks.unregisterBlockStyle("core/image", "rounded");
+
+  // Unregister these options from the Rich Text Editors
+  wp.richText.unregisterFormatType("core/image");
+  wp.richText.unregisterFormatType("core/code");
+  wp.richText.unregisterFormatType("core/keyboard");
 });
