b989ff0bd676f44d748105d37d1ac8efc180a814
Morgan Baker <morgan@strutdigital.co.uk>
2021-11-30 16:22:30 +0000

    Added navigation functions

--- template-parts/header/site-nav.php
+++ template-parts/header/site-nav.php
@@ -14,15 +14,84 @@
         </div>
 
         <?php 
-        wp_nav_menu(
-            array(
-                'theme_location'    => 'primary',
-                'menu_class'        => 'menu-wrapper',
-                'container_class'   => 'primary-menu-container',
-                'items_wrap'        => '<ul id="primary-menu-list" class="%2$s">%3$s</ul>',
-                'fallback_cb'       => false,
-            )
-            );
+
+        $menuItems = wp_get_nav_menu_items( 'header-navigation' );
+
+        if($menuItems) {
+            $menu = array();
+
+            foreach($menuItems as $menuItem) {
+                $tmpItem = array(
+                    'Text'  => $menuItem->title,
+                    'Link'  => $menuItem->url,
+                    'ObjID' => $menuItem->object_id
+                );
+
+                populateNav($menu, $menuItem->menu_item_parent, $menuItem->ID, $tmpItem);
+            }
+
+            echo '<ul>';
+
+            foreach($menu as $menuItm) {
+                paintNav($menuItm);
+            }
+
+            echo '</ul>';
+        }
         ?>
     </nav>
 <?php endif; ?>
+
+<?php 
+
+function paintNav($menuItm) {
+
+    if(isset($menuItm['Children']) && is_array($menuItm['Children']) && count($menuItm['Children']) > 0) {
+
+        echo '<li class="navigation__hasChildren">';
+            echo '<a href="' . $menuItm['Item']['Link'] . '">
+                    ' . $menuItm['Item']['Text'] . '
+                  </a>';
+
+            // SUB ITEMS
+            echo '<div class="navigation__main--sub">
+                    <ul>';
+
+                    foreach($menuItm['Children'] as $childMenu) {
+                        paintNav($childMenu);
+                    }
+
+              echo '</ul>
+                  </div>
+              </li>';
+    } else {
+        echo '<li>
+                <a href="' . $menuItm['Item']['Link'] . '">
+                    ' . $menuItm['Item']['Text'] . '
+                </a>
+              </li>';
+    }
+}
+
+function populateNav(&$menu, $parentID, $itemID, $tmpItem) {
+
+    if($parentID == 0) {
+
+        // TOP LEVEL
+        $menu[$itemID] = array('Item' => $tmpItem, 'Children' => array());
+    } else {
+        if(isset($menu[$parentID])) {
+            //NEXT LEVEL
+            $menu[$parentID]['Children'][$itemID] = array('Item' => $tmpItem, 'Children' => array());
+        } else {
+            // LOWER LEVELS RECURSIVE
+            foreach($menu as $mnItmK => $mnItmV) {
+                if(count($menu[$mnItmK]['Children']) > 0) {
+                    populateNav($menu[$mnItmK]['Children'], $parentID, $itemID, $tmpItem);
+                }
+            }
+        }
+    }
+}
+
+?>
